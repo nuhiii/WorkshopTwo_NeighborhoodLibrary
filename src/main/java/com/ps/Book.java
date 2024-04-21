4d8f758afdfebbc46dbca8e1ff0588102cc8ede9
Nuhirath Rafthia <nr2793@columbia.edu>
2024-04-21 16:52:32 -0400

    set up template for book class

---
+++ src/main/java/com/ps/Book.java
@@ -0,0 +1,24 @@
+package com.ps;
+
+public class Book {
+    //class member variables
+    private int id;
+    private String isbn;
+    private String title;
+    private boolean isCheckedOut;
+    private String checkedOutTo;
+
+    //Constructor
+
+    //Setters
+
+    //Getters
+
+    //class methods to check out / check in
+    public void checkOut(String name){
+
+    }
+    public void checkIn(){
+
+    }
+}
